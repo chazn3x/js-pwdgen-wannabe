1f841c9d8bc7aaaa6c1d07f0d3f722f12ec71b16
Giovanni <gio.a.98@gmail.com>
2021-11-04 13:11:30 +0100

    fine esercizio con console log

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const firstName = prompt('Qual è il tuo nome?');
+const lastName = prompt('Qual è il tuo cognome?');
+const favColor = prompt('Qual è il tuo colore preferito?');
+
+console.log(firstName + lastName + favColor + '21');
