7a15a9ca5cda5cef0e3a9d59f5aa9cb701d5070b
Giovanni <gio.a.98@gmail.com>
2021-11-04 13:20:18 +0100

    aggiunta const per password

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const firstName = prompt('Qual è il tuo nome?');
 const lastName = prompt('Qual è il tuo cognome?');
 const favColor = prompt('Qual è il tuo colore preferito?');
+const password = firstName + lastName + favColor + '21';
 
-console.log(firstName + lastName + favColor + '21');
+console.log(password);
 
-document.getElementById('password').innerHTML = firstName + lastName + favColor + '21';
+document.getElementById('password').innerHTML = 'La tua password è: ' + password;
